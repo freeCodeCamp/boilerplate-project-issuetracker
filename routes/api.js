6350e6c77399b5cca0ee74455bafd47977f7a525
TaraBryn <tbstahler@gmail.com>
2019-11-11 09:57:11 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -12,8 +12,6 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
 module.exports = function (app) {
 
   app.route('/api/issues/:project')

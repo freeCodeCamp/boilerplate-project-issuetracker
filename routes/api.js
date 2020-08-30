63955931fe41229a9ac17a73f951f779a8de13d8
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-30 20:59:55 +0100

    Added code to save a new issue and fetch all issues

--- routes/api.js
+++ routes/api.js
@@ -11,21 +11,55 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
-
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const CONNECTION_STRING = process.env.DB;
 
 module.exports = function (app) {
 
+MongoClient.connect(CONNECTION_STRING, 
+  function(err, client) {
+  if(err){ console.log(err); }
+
+  const db = client.db('issuestracker');
+
   app.route('/api/issues/:project')
-  
+
     .get(function (req, res){
       var project = req.params.project;
-      
+
+      db.collection(project)
+        .find({}).toArray((err,docs)=>{
+          if(err){ console.log(err); }
+	  res.json(docs);
+        });      
     })
     
     .post(function (req, res){
       var project = req.params.project;
       
+      const issue_title = req.body.issue_title;
+      const issue_text = req.body.issue_text;
+      const created_by = req.body.created_by;
+      const assigned_to = req.body.assigned_to || '';
+      const status_text = req.body.status_text || '';
+      const created_on = new Date();
+      const updated_on = new Date();
+      const open = true;
+
+      db.collection(project)
+        .insertOne({
+          issue_title,
+    	  issue_text,
+          created_by,
+          assigned_to,
+     	  status_text,
+          created_on,
+          updated_on,
+          open
+        }, (err, result)=>{
+          if(err){console.log(err);}
+          //console.log(result);
+  	  res.json(result.ops[0]);
+        });
     })
     
     .put(function (req, res){
@@ -37,5 +71,11 @@ module.exports = function (app) {
       var project = req.params.project;
       
     });
-    
+
+    app.use((req,res,next)=>{
+      res.status(404)
+         .type('text')
+         .send('Not Found');
+    });
+  });    
 };

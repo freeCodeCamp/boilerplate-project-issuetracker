2fecb248cbeb7f60861628711f5587bce2cd9544
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-30 21:00:54 +0100

    Removed code for 404 not found middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+require('dotenv').config();
 var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
@@ -40,13 +41,7 @@ fccTestingRoutes(app);
 //Routing for API 
 apiRoutes(app);  
     
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
-
+	
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);

a383d7475af7d24c42ae5eac1246f98e8ba79ae6
Kazeem Kadiri <kazeem.kadiri@gmail.com>
2020-08-30 17:57:26 +0100

    Configured app to use helmet

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+let helmet	= require('helmet');
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -11,6 +12,7 @@ var runner            = require('./test-runner');
 
 var app = express();
 
+app.use(helmet());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only

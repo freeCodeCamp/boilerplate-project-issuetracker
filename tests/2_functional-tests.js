e27d1b38c92e16bbb46dded9360bf5224e1bac79
SaintPeter <rex.schrader@gmail.com>
2020-10-17 10:09:39 -0700

    Initial Refactor

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,10 +6,10 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+const server = require('../server');
 
 chai.use(chaiHttp);
 

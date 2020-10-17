8fc7aa8098c64f8908f349778baae882df978f1a
SaintPeter <rex.schrader@gmail.com>
2020-10-17 13:15:33 -0700

    Add `dotenv` package

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const expect      = require('chai').expect;
 const cors        = require('cors');
+require('dotenv').config();
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
